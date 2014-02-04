9374aa90793b4c40979f3bb2b5d950316499613f
Daniel Barreto <9leinad0@gmail.com>
2014-02-04 01:43:47 -0500

    Add CookieHandler and List<HttpCookie>  now it's working, this will need improve UI design

--- orcemovil/src/main/java/com/fiis/orcemovil/LoginActivity.java
+++ orcemovil/src/main/java/com/fiis/orcemovil/LoginActivity.java
@@ -13,11 +13,11 @@ import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.fiis.orcemovil.util.CookieManager;
-
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -25,12 +25,17 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.net.CookiePolicy;
+import java.net.HttpCookie;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
 
 
 /**
@@ -38,14 +43,6 @@ import java.security.NoSuchAlgorithmException;
  * well.
  */
 public class LoginActivity extends Activity {
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello",
-            "bar@example.com:world"
-    };
 
     /**
      * The default email to populate the email field with.
@@ -56,6 +53,8 @@ public class LoginActivity extends Activity {
      * Keep track of the login task to ensure we can cancel it if requested.
      */
     private UserLoginTask mAuthTask = null;
+    private UserCourseTask mCourseTask = null;
+    private UserScoreTask mScoreTask = null;
 
     // Values for email and password at the time of the login attempt.
     private String mEmail;
@@ -69,6 +68,9 @@ public class LoginActivity extends Activity {
     private TextView mLoginStatusMessageView;
 
     private static CookieManager cm;
+    private Button pyde_button;
+    private Button clear_button;
+    private EditText response_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,9 +79,7 @@ public class LoginActivity extends Activity {
         setContentView(R.layout.activity_login);
 
         // Set up the login form.
-        mEmail = getIntent().getStringExtra(EXTRA_EMAIL);
         mEmailView = (EditText) findViewById(R.id.username);
-        mEmailView.setText(mEmail);
 
         mPasswordView = (EditText) findViewById(R.id.password);
         mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
@@ -111,12 +111,35 @@ public class LoginActivity extends Activity {
                 }
             }
         });
+        pyde_button = (Button) findViewById(R.id.pyde_button);
+        pyde_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                courses();
+            }
+        });
+
+        clear_button = (Button) findViewById(R.id.clear_button);
+        clear_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                response_text.setText("");
+                clear_button.setVisibility(0);
+            }
+        });
+
+        response_text = (EditText) findViewById(R.id.response_text);
+        response_text.setText("");
     }
 
+    //@TODO Hacer que Froyo tambien pueda
+    @TargetApi(Build.VERSION_CODES.GINGERBREAD)
     @Override
     protected void onResume() {
         super.onResume();
         cm = new CookieManager();
+        cm.setCookiePolicy(CookiePolicy.ACCEPT_ALL);
+        CookieHandler.setDefault(cm);
     }
 
     @Override
@@ -178,7 +201,7 @@ public class LoginActivity extends Activity {
 
         Log.d("post_send_data", data);
 
-        String text = "http://www.orce.uni.edu.pe/m/api/login.json.php";
+        String text = getString(R.string.url_login);
 
         String[] params = {text,data};
         BufferedReader reader=null;
@@ -243,9 +266,9 @@ public class LoginActivity extends Activity {
      * the user.
      */
     public class UserLoginTask extends AsyncTask<String, Void, String> {
+        @TargetApi(Build.VERSION_CODES.GINGERBREAD)
         @Override
         protected String doInBackground(String... params) {
-            // TODO: attempt authentication against a network service.
             String url_str = params[0];
             String data = params[1];
             Log.d("post_line","" + new Exception().getStackTrace()[0].getLineNumber());
@@ -259,20 +282,15 @@ public class LoginActivity extends Activity {
                 Log.d("post_line","" + new Exception().getStackTrace()[0].getLineNumber());
                 HttpURLConnection conn = (HttpURLConnection) url.openConnection();
                 Log.d("post_line","" + new Exception().getStackTrace()[0].getLineNumber());
-                //cm.setCookies(conn);
                 Log.d("post_line","" + new Exception().getStackTrace()[0].getLineNumber());
                 conn.setRequestMethod("POST");
                 conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=utf-8");
                 conn.setUseCaches (false);
+
+                //Connect
                 conn.setDoInput(true);
                 conn.setDoOutput(true);
 
-                /*
-                URLConnection conn = url.openConnection();
-                conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-
-                conn.setDoOutput(true);
-                */
                 DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
                 wr.writeBytes(data);
                 wr.flush();
@@ -281,8 +299,17 @@ public class LoginActivity extends Activity {
                 DataInputStream reader = new DataInputStream(conn.getInputStream());
                 StringBuffer sb = new StringBuffer();
                 String line = null;
-                cm.storeCookies(conn);
                 Log.d("post_cookie", cm.toString());
+
+                List<HttpCookie> cookies = cm.getCookieStore().getCookies();
+                for (HttpCookie cookie: cookies)
+                {
+                    Log.d("post","Name = " + cookie.getName());
+                    Log.d("post","Name = " + cookie.getName());
+                    Log.d("post","Value = " + cookie.getValue());
+                    Log.d("post","Lifetime (seconds) = " + cookie.getMaxAge());
+                    Log.d("post","Path = " + cookie.getPath());
+                }
 
                 // Read Server Response
                 while((line = reader.readLine()) != null)
@@ -301,16 +328,6 @@ public class LoginActivity extends Activity {
                 Log.e("post_exception", e.getMessage());
             }
 
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    Log.d("post_line","" + new Exception().getStackTrace()[0].getLineNumber());
-                    return "true";
-                }
-            }
-
-            // TODO: register the new account here.
             Log.d("post_line","" + new Exception().getStackTrace()[0].getLineNumber());
             return "false";
         }
@@ -322,6 +339,8 @@ public class LoginActivity extends Activity {
             Log.d("post_result",result);
             mAuthTask = null;
             showProgress(false);
+            response_text.setText(response_text.getText()+"\n\n\n"+result);
+            clear_button.setVisibility(1);
 
             if (result != "" || result == "true") {
                 try{
@@ -329,16 +348,7 @@ public class LoginActivity extends Activity {
                     Boolean error = (Boolean) myObject.get("error");
                     if (!error) {
                         Log.d("post_login", "CONGRATULATIONS!");
-                        //@TODO Guardar Cookie para pedir datos o buscar otra forma
-                        String data = URLEncoder.encode("CURSO", "UTF-8")
-                                + "=" + URLEncoder.encode("GP515", "UTF-8");
-
-                        data += "&" + URLEncoder.encode("SECCION", "UTF-8") + "="
-                                + URLEncoder.encode("U", "UTF-8");
-                        String[] params = {"http://www.orce.uni.edu.pe/m/api/notas.json.php",data};
-                        mAuthTask = new UserLoginTask();
-                        mAuthTask.execute(params);
-                        finish();
+                        pyde_button.setVisibility(1);
                     }
                 } catch (Exception e) {
                     Log.e("post_exception", e.getMessage());
@@ -353,6 +363,219 @@ public class LoginActivity extends Activity {
         @Override
         protected void onCancelled() {
             mAuthTask = null;
+            showProgress(false);
+            Log.d("post_cancel","CANCELED!");
+        }
+    }
+
+    public class UserCourseTask extends AsyncTask<String, Void, String> {
+        @TargetApi(Build.VERSION_CODES.GINGERBREAD)
+        @Override
+        protected String doInBackground(String... params) {
+            String url_str = params[0];
+            Log.d("post_line","" + new Exception().getStackTrace()[0].getLineNumber());
+            Log.d("post_url",url_str);
+            try {
+
+                // Defined URL  where to send data
+                URL url = new URL(url_str);
+
+                // Send GET request
+                Log.d("post_line","" + new Exception().getStackTrace()[0].getLineNumber());
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                Log.d("post_line","" + new Exception().getStackTrace()[0].getLineNumber());
+                conn.setRequestMethod("GET");
+                conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=utf-8");
+                conn.setUseCaches (false);
+
+                //Connect
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                // Get the server response
+
+                DataInputStream reader = new DataInputStream(conn.getInputStream());
+                StringBuffer sb = new StringBuffer();
+                String line = null;
+                Log.d("post_cookie", cm.toString());
+
+                List<HttpCookie> cookies = cm.getCookieStore().getCookies();
+                for (HttpCookie cookie: cookies)
+                {
+                    Log.d("post","Name = " + cookie.getName());
+                    Log.d("post","Name = " + cookie.getName());
+                    Log.d("post","Value = " + cookie.getValue());
+                    Log.d("post","Lifetime (seconds) = " + cookie.getMaxAge());
+                    Log.d("post","Path = " + cookie.getPath());
+                }
+
+                // Read Server Response
+                while((line = reader.readLine()) != null)
+                {
+                    // Append server response in string
+                    sb.append(line + "\n");
+                }
+
+                String text = sb.toString();
+                Log.d("post_line","" + new Exception().getStackTrace()[0].getLineNumber());
+                return text;
+
+            } catch (MalformedURLException e) {
+                Log.e("post_exception", e.getMessage());
+            } catch (IOException e) {
+                Log.e("post_exception", e.getMessage());
+            }
+            Log.d("post_line","" + new Exception().getStackTrace()[0].getLineNumber());
+            return "false";
+        }
+
+        @Override
+        protected void onPostExecute(final String result) {
+
+            Log.d("post_line","" + new Exception().getStackTrace()[0].getLineNumber());
+            Log.d("post_result", result);
+            mCourseTask = null;
+            showProgress(false);
+            response_text.setText(response_text.getText()+"\nXXXXXXXXXXXXXXXXXXX\n"+result);
+            clear_button.setVisibility(1);
+
+            if (result != "" || result == "true") {
+                try{
+                    JSONObject myObject = new JSONObject(result);
+                    Boolean error = (Boolean) myObject.get("error");
+                    if (!error) {
+                        Log.d("post_login", "CONGRATULATIONS!");
+
+                        JSONObject data = (JSONObject) myObject.get("data");
+                        JSONArray cursos = data.getJSONArray("cursos");
+                        for (int i = 0; i < cursos.length(); i++) {
+                            JSONObject curso = cursos.getJSONObject(i);
+                            Log.d("post_condition",curso.getString("condicion"));
+                            if (curso.getString("condicion").equals("N")) {
+                                Log.d("post_line","" + new Exception().getStackTrace()[0].getLineNumber());
+                                String codigo = (String) curso.get("codigo");
+                                String seccion = (String) curso.get("seccion");
+                                score(codigo, seccion);
+                            }
+                            Log.d("post_line","" + new Exception().getStackTrace()[0].getLineNumber());
+                        }
+                        pyde_button.setVisibility(1);
+                    }
+                } catch (Exception e) {
+                    Log.e("post_exception", e.getMessage());
+                }
+            } else {
+                Log.d("post_login", "FAIL!");
+                mPasswordView.setError(getString(R.string.error_incorrect_password));
+                mPasswordView.requestFocus();
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            mCourseTask = null;
+            showProgress(false);
+            Log.d("post_cancel","CANCELED!");
+        }
+    }
+
+    public class UserScoreTask extends AsyncTask<String, Void, String> {
+        @TargetApi(Build.VERSION_CODES.GINGERBREAD)
+        @Override
+        protected String doInBackground(String... params) {
+            String url_str = params[0];
+            String data = params[1];
+            Log.d("post_line","" + new Exception().getStackTrace()[0].getLineNumber());
+            Log.d("post_url",url_str);
+            Log.d("post_data",data);
+            try {
+                // Defined URL  where to send data
+                URL url = new URL(url_str);
+
+                // Send POST data request
+                Log.d("post_line","" + new Exception().getStackTrace()[0].getLineNumber());
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                Log.d("post_line","" + new Exception().getStackTrace()[0].getLineNumber());
+                Log.d("post_line","" + new Exception().getStackTrace()[0].getLineNumber());
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=utf-8");
+                conn.setUseCaches (false);
+
+                //Connect
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+                wr.writeBytes(data);
+                wr.flush();
+                // Get the server response
+
+                DataInputStream reader = new DataInputStream(conn.getInputStream());
+                StringBuffer sb = new StringBuffer();
+                String line = null;
+                Log.d("post_cookie", cm.toString());
+
+                List<HttpCookie> cookies = cm.getCookieStore().getCookies();
+                for (HttpCookie cookie: cookies)
+                {
+                    Log.d("post","Name = " + cookie.getName());
+                    Log.d("post","Name = " + cookie.getName());
+                    Log.d("post","Value = " + cookie.getValue());
+                    Log.d("post","Lifetime (seconds) = " + cookie.getMaxAge());
+                    Log.d("post","Path = " + cookie.getPath());
+                }
+
+                // Read Server Response
+                while((line = reader.readLine()) != null)
+                {
+                    // Append server response in string
+                    sb.append(line + "\n");
+                }
+
+                String text = sb.toString();
+                Log.d("post_line","" + new Exception().getStackTrace()[0].getLineNumber());
+                return text;
+
+            } catch (MalformedURLException e) {
+                Log.e("post_exception", e.getMessage());
+            } catch (IOException e) {
+                Log.e("post_exception", e.getMessage());
+            }
+
+            Log.d("post_line","" + new Exception().getStackTrace()[0].getLineNumber());
+            return "false";
+        }
+
+        @Override
+        protected void onPostExecute(final String result) {
+
+            Log.d("post_line","" + new Exception().getStackTrace()[0].getLineNumber());
+            Log.d("post_result",result);
+            mScoreTask = null;
+            showProgress(false);
+            response_text.setText(response_text.getText()+"\n==================\n"+result);
+            clear_button.setVisibility(1);
+
+            if (result != "" || result == "true") {
+                try{
+                    JSONObject myObject = new JSONObject(result);
+                    Boolean error = (Boolean) myObject.get("error");
+                    if (!error) {
+                        Log.d("post_login", "CONGRATULATIONS!");
+                    }
+                } catch (Exception e) {
+                    Log.e("post_exception", e.getMessage());
+                }
+            } else {
+                Log.d("post_login", "FAIL!");
+                mPasswordView.setError(getString(R.string.error_incorrect_password));
+                mPasswordView.requestFocus();
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            mScoreTask = null;
             showProgress(false);
             Log.d("post_cancel","CANCELED!");
         }
@@ -380,5 +603,22 @@ public class LoginActivity extends Activity {
             Log.e("post_exception", e.getMessage());
         }
         return "";
+    }
+
+    //Obtener notas del curso PyDE
+    public void score(String codigo, String seccion) throws UnsupportedEncodingException {
+        String data = URLEncoder.encode("CURSO", "UTF-8")
+                + "=" + URLEncoder.encode(codigo, "UTF-8");
+
+        data += "&" + URLEncoder.encode("SECCION", "UTF-8") + "="
+                + URLEncoder.encode(seccion, "UTF-8");
+        String[] params = {getString(R.string.url_notas),data};
+        mScoreTask = new UserScoreTask();
+        mScoreTask.execute(params);
+    }
+
+    public void courses() {
+        mCourseTask = new UserCourseTask();
+        mCourseTask.execute(getString(R.string.url_cursos));
     }
 }
